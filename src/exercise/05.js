4717c5c0f07f9bceea8331430771573822c48c6c
macosgrove <maryanne@covidence.org>
2024-10-18 17:50:30 +1100

    Exercise 5.1

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,6 +4,7 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
+// use-toggle file
 const callAll = (...fns) => (...args) => fns.forEach(fn => fn?.(...args))
 
 function toggleReducer(state, {type, initialState}) {
@@ -52,25 +53,21 @@ function useToggle({initialOn = false, reducer = toggleReducer} = {}) {
   }
 }
 
+// export {useToggle, toggleReducer}
+
+// App file
+
+// import {useToggle, toggleReducer} from './use-toggle'
+
 function App() {
   const [timesClicked, setTimesClicked] = React.useState(0)
   const clickedTooMuch = timesClicked >= 4
 
   function toggleStateReducer(state, action) {
-    switch (action.type) {
-      case 'toggle': {
-        if (clickedTooMuch) {
-          return {on: state.on}
-        }
-        return {on: !state.on}
-      }
-      case 'reset': {
-        return {on: false}
-      }
-      default: {
-        throw new Error(`Unsupported type: ${action.type}`)
-      }
+    if (action.type === 'toggle' && timesClicked >= 4) {
+      return {on: state.on}
     }
+    return toggleReducer(state, action)
   }
 
   const {on, getTogglerProps, getResetterProps} = useToggle({
